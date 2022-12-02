53b7c9caadf562f56e8ce5edafdbc0c1323adab6
patrykrossa <patrros601@student.polsl.pl>
2022-12-02 20:43:02 +0100

    authenticate provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,12 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme/extendedTheme";
 import { AppWrapper } from "./components/global/AppWrapper/appWrapper";
+import { AuthProvider } from "./providers/Authenticate";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <AppWrapper />
+    <AuthProvider>
+      <AppWrapper />
+    </AuthProvider>
   </ChakraProvider>
 );

ef361fe83bbe6d5eaca7483c5e6509532c1ecdec
damiansarnecki <xdamiansarnecki@gmail.com>
2022-12-03 08:03:32 +0100

    save

--- frontend/src/components/pages/Tasks/tasks.tsx
+++ frontend/src/components/pages/Tasks/tasks.tsx
@@ -12,11 +12,32 @@ import {
   Input,
   useDisclosure,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import ReactDatePicker from "react-datepicker";
 import { tasks } from "../Calendar/tasks";
+
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from "@chakra-ui/react";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 import { Task } from "./Task/task";
 
 export const Tasks = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [dueDate, setDueDate] = useState(new Date());
+  const [taskName, setTaskName] = useState<string>("Task");
+  const [urgency, setUrgency] = useState<number>(1);
+
+  const addNewTask = () => {
+    console.log(dueDate, taskName, urgency);
+  };
 
   return (
     <Flex flexDir="column" bg="f8f8f8" maxH="100vh" overflowY="scroll">
@@ -54,17 +75,53 @@ export const Tasks = () => {
         <DrawerContent>
           <DrawerCloseButton onClick={onClose} />
 
-          <DrawerBody p="30px" gap="20px">
-			  <Box fontSize='25px' fontWeight='bold'>Add new Task</Box>
-			  <Input placeholder={"Task name"}></Input>
-		  </DrawerBody>
+          <DrawerBody
+            p="30px"
+            gap="20px"
+            display="flex"
+            flexDir="column"
+            gridGap="40px"
+          >
+            <Box fontSize="25px" fontWeight="bold">
+              Add new Task
+            </Box>
+            <Flex flexDir="column" gap="12px">
+              <Box>Task name</Box>
+              <Input
+                placeholder={"Task name"}
+				defaultValue={"Task name"}
+                onChange={(e) => setTaskName(e.target.value)}
+              ></Input>
+            </Flex>
+            <Flex flexDir="column" gap="12px">
+              <Box>Due date</Box>
+              <ReactDatePicker
+                selected={dueDate}
+                onChange={(date: Date) => setDueDate(date)}
+              />
+            </Flex>
+            <Flex flexDir="column" gap="12px">
+              <Box>Urgency</Box>
+              <Slider
+                aria-label="slider-ex-1"
+                max={5}
+                defaultValue={1}
+                min={1}
+                onChange={(val) => setUrgency(val)}
+              >
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+            </Flex>
+          </DrawerBody>
 
           <DrawerFooter>
             <Button
               variant="outline"
               mr={3}
               onClick={() => {
-               
                 onClose();
               }}
             >
@@ -73,6 +130,7 @@ export const Tasks = () => {
             <Button
               colorScheme="blue"
               onClick={() => {
+                addNewTask();
                 onClose();
               }}
             >
